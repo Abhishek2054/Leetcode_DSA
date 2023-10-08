b4424cfcedfb99bef0d95f7506f69da095d9a7c7
Abhishek Mishra <abhishekmishra2115@gmail.com>
2023-10-08 11:41:18 +0530

    added few easy questions based on array.

---
+++ Arrays/ShuffleTheArray.java
@@ -0,0 +1,24 @@
+// Given the array nums consisting of 2n elements 
+// in the form [x1,x2,...,xn,y1,y2,...,yn].
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+// Example:
+// Input: nums = [2,5,1,3,4,7], n = 3
+// Output: [2,3,5,4,1,7] 
+// Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 
+// then the answer is [2,3,5,4,1,7].
+
+
+
+// Brute force approach
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[nums.length];
+        int pointer= n, j = 0;
+        for( int i = 0; i<nums.length; i++ ){
+            ans[i] = nums[j++];
+            ans[++i] = nums[pointer++];
+        }
+        return ans;
+    }
+}
